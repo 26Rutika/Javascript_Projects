ff1a351f7608ab73501a2ffc01c46b7434c98214
Rutika <rutikaghawate26@gmail.com>
2024-06-19 01:02:45 +0530

    add

---
+++ project1/app.js
@@ -0,0 +1,8 @@
+let input=document.querySelector("input");
+btn=document.querySelector("button");
+btn.addEventListener("click",function()
+{
+    // console.log(input.value);
+    let ran=Math.floor(Math.random()*2)+1;
+    console.log(ran);
+})
